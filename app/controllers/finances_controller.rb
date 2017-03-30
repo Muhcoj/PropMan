169d3613623ec4a6d9edbec41c355728ef4f947b
Muhcoj <jochum.de.bueger@gmail.com>
2017-03-30 19:46:06 +0200

    Using BDD to create Finances and Implement CRUD

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,5 +1,21 @@
 class FinancesController < ApplicationController
 	def index
+		
+	end
+
+	def new
 		@finance = Finance.new
+	end
+
+	def create
+		@finance = Finance.new(params.require(:finance).permit(:year, :month, :payment_due))
+    
+    @finance.save
+
+    redirect_to @finance
+	end
+
+	def show
+		@finance = Finance.find(params[:id])
 	end
 end
--- spec/features/finance_spec.rb
+++ spec/features/finance_spec.rb
@@ -12,4 +12,23 @@ describe 'navigate' do
 			expect(page).to have_content(/Finance/)
 		end
 	end
+
+	describe 'creation' do
+		it 'has a new form that can be reached' do
+			visit new_finance_path
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'can be created from new form page' do
+			visit new_finance_path
+
+      fill_in 'finance[year]', with: "2017"
+      fill_in 'finance[month]', with: "January"
+      fill_in 'finance[payment_due]', with: 40.45
+
+      click_on "Save"
+
+      expect(page).to have_content("2017")
+		end
+	end
 end
