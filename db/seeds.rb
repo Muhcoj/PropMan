0f82f7015a3e0d728186f63278f2c3e6bcc8454e
Muhcoj <jochum.de.bueger@gmail.com>
2017-03-30 17:16:33 +0200

    Created seed file with default Finance content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do |finance|
+	Finance.create!(year: "#{finance} year", month: "#{finance} month", payment_due: 45.80)
+end
+
+puts "100 Finances have been created"
