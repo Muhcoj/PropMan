74f8fec5587ae0927e16e6e61eb92d798c1ab0c8
Muhcoj <jochum.de.bueger@gmail.com>
2017-08-21 18:28:52 +0200

    Fixed bug in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,18 +2,18 @@ class UserMailer < ApplicationMailer
 	def new_invoice (user, finance)
 		@user = user
 		@finance = finance
-		mail(from: 'jochum.de.bueger@gmail.com', to: @user.email, subject: 'You have a new open invoice from Sudetenlandweg, Sankt Andreasberg.').deliver_now
+		mail(from: 'jochum.de.bueger@gmail.com', to: @user.email, subject: 'You have a new open invoice from Sudetenlandweg, Sankt Andreasberg.')
 	end
 
 	def invoice_paid (user, finance)
 		@user = user
-		mail(from: 'jochum.de.bueger@gmail.com', to: @user.email, subject: 'Your invoice from Sudetenlandweg, Sankt Andreasberg.').deliver_now
+		mail(from: 'jochum.de.bueger@gmail.com', to: @user.email, subject: 'Your invoice from Sudetenlandweg, Sankt Andreasberg.')
 	end
 
 	def invoice_reminder (user, finance)
 		@user = user
 		@finance = finance
-		mail(from: 'jochum.de.bueger@gmail.com', to: @user.email, subject: 'Your invoice from Sudetenlandweg, Sankt Andreasberg.').deliver_now
+		mail(from: 'jochum.de.bueger@gmail.com', to: @user.email, subject: 'Your invoice from Sudetenlandweg, Sankt Andreasberg.')
 	end
 
 end
